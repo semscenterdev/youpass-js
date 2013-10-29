9c5526186e452fdea2af326868f7c8751bb63945
GeekyGamer14 <tj.201@me.com>
2013-10-28 20:32:31 -0700

    some commands

---
+++ get/yt-getter.js
@@ -0,0 +1,15 @@
+youpass = [];
+
+youpass.get = function(url, params){
+	if(url.indexOf("youtube.com") == -1){
+		return 'Invalid YouTube URL.';
+	}else{
+		if(params == undefined){
+			params == '';
+		}
+		var a = url.replace("watch?v=","v/");
+		var b = '<iframe width="640" height="360" frameborder="0" src="' + a + '&' + params + '"></iframe>';
+		var c = b.replace(/,/g,"&");
+		return c;
+	}
+}
