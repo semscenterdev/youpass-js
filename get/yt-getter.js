a3c285bc5c212a5987f038077f1fd67032b55ae0
GeekyGamer14 <tj.201@me.com>
2014-01-09 22:32:21 -0800

    how about we just use the get parameter lol

--- get/yt-getter.js
+++ get/yt-getter.js
@@ -8,22 +8,17 @@ youpass.get = function(url, params){
 			params == '';
 		}
 		var a = url.replace("watch?v=","v/");
-		var b = '<iframe width="640" height="360" frameborder="0" src="' + a + '&' + params + '"></iframe>';
+		var b = '<iframe width="640" height="360" type="application/x-shockwave-flash" frameborder="0" src="' + a + '&' + params + '"></iframe>';
 		var c = b.replace(/,/g,"&");
 		return c;
 	}
 }
 
-// very glitchy, if you have access to php, use that instead.
+// use the php version instead of this.
 youpass.strip_id = function(url){
 	if(url.indexOf("youtube.com") == -1){
 		return 'Invalid YouTube URL.';
 	}else{
-		var a = url.replace("http://www.youtube.com/watch?v=","");
-		var b = url.replace("https://www.youtube.com/watch?v=","");
-		var c = a.replace("youtube","");
-		var d = b.replace("https","");
-		var e = c.replace("watch?v=","");
-		return e;
+		return url.substr(url.indexOf("=") + 1);
 	}
 }
